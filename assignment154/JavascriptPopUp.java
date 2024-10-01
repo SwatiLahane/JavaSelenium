8d3992cd181d3bfa3f3b44ea6b9c9cb2d1289712
Swati lahane <101633697+SwatiLahane@users.noreply.github.com>
2024-10-01 12:17:27 +0530

    Update JavascriptPopUp.java

--- assignment154/JavascriptPopUp.java
+++ assignment154/JavascriptPopUp.java
@@ -1,8 +1,12 @@
 package assignment154;
 
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class JavascriptPopUp
 {   
@@ -14,10 +18,16 @@ public class JavascriptPopUp
 		 
 		 WebDriver driver = new ChromeDriver();
 		 driver.manage().window().maximize();
-		 driver.get("https://grotechminds.com/alert/");
+		 driver.get("https://grotechminds.com/javascript-popup/");
 		 
-		 driver.findElement(By.linkText("Received1"));
-		 driver.switchTo().alert().dismiss();
+		 driver.findElement(By.xpath("//button[@class='btnjs']"));
+		 
+		  WebDriverWait w1 = new  WebDriverWait(driver,Duration.ofSeconds(10));
+		// w1.until(ExpectedConditions.titleIs("Google"));
+		  
+		  WebDriverWait w2 = new WebDriverWait(driver,Duration.ofSeconds(10));
+		  w2.until(ExpectedConditions.titleIs("JavaScript Popup - GroTechMinds"));
+		 driver.switchTo().alert().accept();
 		 
 		 String s =  driver.switchTo().alert().getText();
 		 System.out.println(s);
